a2139207ceebd24c4b9fb21af775decddfce3049
igoridragonYT <igoridragon2007@gmail.com>
2026-09-16 02:28:42 +0300

    Create src/main/java/com/igoridragon/avatar/AvatarMod.java

---
+++ src/main/java/com/igoridragon/avatar/AvatarMod.java
@@ -0,0 +1,13 @@
+package com.igoridragon.avatar;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(AvatarMod.MOD_ID)
+public class AvatarMod {
+
+    public static final String MOD_ID = "avatar";
+
+    public AvatarMod() {
+        System.out.println("Avatar: Frontiers of Pandora - Dragon Edition loaded!");
+    }
+}
