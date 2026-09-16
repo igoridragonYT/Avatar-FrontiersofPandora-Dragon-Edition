86a6a967265d9d80414d4b26ed25b1d9b849b0da
igoridragonYT <igoridragon2007@gmail.com>
2026-09-16 03:44:36 +0300

    Update AvatarMod.java

--- src/main/java/com/igoridragon/avatar/AvatarMod.java
+++ src/main/java/com/igoridragon/avatar/AvatarMod.java
@@ -1,8 +1,6 @@
 package com.igoridragon.avatar;
 
-import com.igoridragon.avatar.entity.ModEntities;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(AvatarMod.MOD_ID)
 public class AvatarMod {
@@ -10,10 +8,6 @@ public class AvatarMod {
     public static final String MOD_ID = "avatar";
 
     public AvatarMod() {
-        var modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        ModEntities.ENTITY_TYPES.register(modEventBus);
-
         System.out.println("Avatar: Frontiers of Pandora - Dragon Edition loaded!");
     }
 }
